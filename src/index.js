01dfcdd9b3c38264ff425c72396d114732c93112
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-02-23 00:22:01 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array) {
+    return 0
+  }
+  let min = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    (array[i] < min || !min) && (min = array[i])
+  }
+
+  return min
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array) {
+    return 0
+  }
+  let max = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    max <= array[i] && (max = array[i])
+  }
+
+  return max
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+exports.avg = function avg(array) {
+  if (!array) {
+    return 0
+  }
+  
+  let avg = 0;
+  let divider = 0
+
+  for (let i = 0; i < array.length; i++) {
+    avg += (array[i])
+    divider = (array.length)
+  }
+
+  return avg / divider
 }
