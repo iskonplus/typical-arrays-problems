8883490048481fb9b3dcf7d6bc1dc2ec1c7acc32
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-02-23 21:52:52 +0300

    fix: change the check of arrays to fix the error

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
 
 exports.min = function min(array) {
-  if (!array) {
-    return 0
-  }
-  let min = 0;
-
-  for (let i = 0; i < array.length; i++) {
-    (array[i] < min || !min) && (min = array[i])
-  }
-
-  return min
+  return !array || !array.length ?
+    0 :
+    array.reduce((tottal, el) =>
+      (tottal >= el ? (tottal = el) : tottal));
 }
 
 exports.max = function max(array) {
-  if (!array) {
-    return 0
-  }
-  let max = 0;
-
-  for (let i = 0; i < array.length; i++) {
-    max <= array[i] && (max = array[i])
-  }
-
-  return max
+  return !array || !array.length ?
+    0 :
+    array.reduce((tottal, el) =>
+      (tottal <= el ? (tottal = el) : tottal));
 }
 
 
 exports.avg = function avg(array) {
-  if (!array) {
+  if (!array || !array.length) {
     return 0
   }
   
