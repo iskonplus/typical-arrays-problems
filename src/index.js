2ae717706629e51aa0bcc2f56256d485d3ea3bb6
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-02-23 22:09:33 +0300

    refactor: small changes

--- src/index.js
+++ src/index.js
@@ -15,17 +15,8 @@ exports.max = function max(array) {
 
 
 exports.avg = function avg(array) {
-  if (!array || !array.length) {
-    return 0
-  }
-  
-  let avg = 0;
-  let divider = 0
-
-  for (let i = 0; i < array.length; i++) {
-    avg += (array[i])
-    divider = (array.length)
-  }
-
-  return avg / divider
+  return !array || !array.length ?
+  0 :
+  array.reduce((tottal, el) =>
+      tottal += el) / array.length
 }
